bbef32a308a6be31869df43d13ad296a220f2fef
unknown <vandinh410807@gmail.com>
2023-03-30 00:01:01 +0700

    my changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 var io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chat-real-time-pro.vercel.app',
     },
 });
 // ---------deployment----------
